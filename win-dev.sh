5c3ad0de05c854dac60bdb327af153c33adbfd4a
V1niciusDG <vinicius.dalanora@waterservicestech.com>
2024-06-11 12:40:58 -0300

    configs mac and wind

---
+++ win-dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# GIT_BASH="/c/Program Files/Git/git-bash.exe"
+# dir="/c/Users/vinicius.dalanora/Projetos/hgaweb"
+ 
+ 
+# # Caminho para o executável do Git Bash
+ 
+# # Comandos para iniciar cada serviço
+# proxyreverse="cd $dir/proxyreverse-hgaweb-services && docker-compose up --build"
+# multilangcore="cd $dir/multilangcore-hgaweb-services && yarn dev"
+# auth="cd $dir/auth-hgaweb-services && yarn dev"
+# tenant="cd $dir/tenant-hgaweb-services && yarn dev"
+ 
+# # Abre cada comando em uma nova janela do Git Bash
+# cmd.exe /C start "$GIT_BASH" -c "$proxyreverse; exec bash"
+# cmd.exe /C start "$GIT_BASH" -c "$multilangcore; exec bash"
+# cmd.exe /C start "$GIT_BASH" -c "$auth; exec bash"
+# cmd.exe /C start "$GIT_BASH" -c "$tenant; exec bash"
+
+#comando para rodar
+#chmod +x win-dev.sh
+
+#./win-dev.sh
