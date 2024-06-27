b7d18e87f61d30c9d56bc8f581a20278ff3ef250
João Marcos <joao@johnnydev.com.br>
2024-06-26 21:50:57 -0300

    Add Widgats

--- mac-dev.sh
+++ mac-dev.sh
@@ -2,13 +2,13 @@
 
 dir="/Users/joaomarcos/Projetos/Waterservicestech/hgaweb"
 
-
 proxyreverse="cd $dir/proxyreverse-hgaweb-services && docker-compose up --build"
 multilangcore="cd $dir/multilangcore-hgaweb-services && yarn dev"
 auth="cd $dir/auth-hgaweb-services && yarn dev"
 tenant="cd $dir/tenant-hgaweb-services && yarn dev"
 pages="cd $dir/pages-hgaweb-services && yarn dev"
 web="cd $dir/web-hgaweb-frontend && yarn dev"
+widgets="cd $dir/widgets-hgaweb-services && yarn dev"
 
 
 osascript <<EOF
@@ -19,6 +19,7 @@ tell application "Terminal"
     do script "$tenant"
     do script "$pages"
     do script "$web"
+    do script "$widgets"
     activate
 end tell
 EOF
